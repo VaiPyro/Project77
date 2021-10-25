d4702345c01f398f554b1211e6a5cb5860a37650
VaiPyro <88515547+VaiPyro@users.noreply.github.com>
2021-10-25 18:57:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var first_name="VAIBHAV"
+console.log(first_name)
+var last_name="Sudhir"
+console.log(last_name)
+var full_name=first_name.concat(last_name)
+console.log(full_name)
+var full_name_length=full_name.length
+console.log(full_name_length)
+var lowercase=first_name.toLowerCase()
+console.log(lowercase)
+var uppercase=last_name.toUpperCase()
+console.log(uppercase)
+var squareroot=Math.sqrt(5)
+console.log(squareroot)
+var maximum_number=Math.max(15,10,45,56,78,90,523,465,7890765432,12147,80907,5680796437890,34789^357,3,6568975435)
+console.log(maximum_number)
+var minimum_number=Math.min(457,56,3463,546,546,456,454,6788,857325,69,4)
+console.log(minimum_number)
+var power=Math.pow(4,3)
+console.log(power)
+var a=3
+var b=6
+var add = a+b
+console.log(add)
+var c=5
+var d=8
+var multiply=c*d
+console.log(multiply)
